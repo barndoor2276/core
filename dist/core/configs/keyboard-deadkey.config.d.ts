cab53fff30085ab64b2fa0deb21e616be7cfff62
bbergan <bernard@berganmail.com>
2019-08-06 10:57:54 -0500

    undo delete

--- dist/core/configs/keyboard-deadkey.config.d.ts
+++
@@ -1,5 +0,0 @@
-import { InjectionToken } from '@angular/core';
-import { IKeyboardDeadkeys } from '../interfaces/keyboard-deadkeys.interface';
-declare const MAT_KEYBOARD_DEADKEYS: InjectionToken<IKeyboardDeadkeys>;
-declare const keyboardDeadkeys: IKeyboardDeadkeys;
-export { IKeyboardDeadkeys, MAT_KEYBOARD_DEADKEYS, keyboardDeadkeys };
--- dist/core/pipes/kebab-case.pipe.d.ts
+++
@@ -1,4 +0,0 @@
-import { PipeTransform } from '@angular/core';
-export declare class MatKeyboardKebabCasePipe implements PipeTransform {
-    transform(value: string): string;
-}
